b2fb8e3534bed74c8bd13684d7676dc1d10ab268
guada <guadarojas04@gmail.com>
2026-03-04 13:59:36 -0300

    Creacion de contexto, tipos y validaciones para login y registro, servicio y formulario de login

---
+++ front/src/context/AuthContext.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { IUserSession } from "@/types/types";
+import { useRouter } from "next/navigation";
+import { createContext, useContext, useEffect, useState } from "react";
+
+export interface IAuthContext {
+  userData: IUserSession | null;
+  setUserData: (values: IUserSession | null) => void;
+  isAuthLoading: boolean;
+}
+
+export const AuthContext = createContext<IAuthContext>({
+  userData: null,
+  setUserData: () => {},
+  isAuthLoading: true,
+});
+
+export interface IAuthProvider {
+  children: React.ReactNode;
+}
+
+export const AuthProvider: React.FC<IAuthProvider> = ({ children }) => {
+  const [userData, setUserData] = useState<IUserSession | null>(null);
+  const [isAuthLoading, setIsAuthLoading] = useState<boolean>(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (userData) {
+      localStorage.setItem(
+        "userSession",
+        JSON.stringify({ token: userData.token, user: userData.user }),
+      );
+    }
+  }, [userData]);
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setIsAuthLoading(true);
+    const userData = JSON.parse(localStorage.getItem("userSession")!);
+    setUserData(userData);
+    setIsAuthLoading(false);
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        userData,
+        setUserData,
+        isAuthLoading,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
