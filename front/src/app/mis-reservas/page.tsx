31dc96a1e6097612915a3375e0c81210fab35e37
guada <guadarojas04@gmail.com>
2026-03-09 14:08:31 -0300

    Cambios

--- front/src/app/mis-reservas/page.tsx
+++ front/src/app/mis-reservas/page.tsx
@@ -1,11 +1,9 @@
 import Reserva from "@/components/reserva/Reserva";
 
-
 export default function ReservasPage() {
   return (
     <div className="p-60">
-       <Reserva /> 
+      <Reserva />
     </div>
-  )
+  );
 }
-
